54719180ef992ae773b81de0b07c057c19c1b1a4
Erick. santos <erickblz@outlook.com>
2020-06-05 23:43:37 -0300

    Adicionando programa de idade

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CalculadoraIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Meu nascimento é 20/10/2001");
+            
+            DateTime um = DateTime.Now;
+            DateTime dois = new DateTime(2001, 10, 20);
+
+            TimeSpan tres = um - dois;
+            string tek = dois.ToShortDateString();
+
+            int muitolouco = tres.Days;
+
+            Console.WriteLine($"Eu Nasci em {tek} e ja vivi {muitolouco}dias");
+
+        }
+    }
+}
